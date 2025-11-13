6fd549abeffdb66144bc8d25f48a4dad9d904ad1
guyera <thegraygrimm@graymansurvivaltech.com>
2025-11-13 17:02:32 -0500

    created the scalfolding for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,30 @@
-
+import Image from "next/image";
 const Home = () => {
-return (
-  <>
-    <header>
-  </header>
-  <main>
-    <div className="landing-page-profile-img">
-
-    </div>
-    <div className="intro">
-      <h1>Alec Guyer</h1>
-      <p>Full-Stack Software Developer</p>
-    </div>
-    <div className="short-info">
-      
-    </div>
-  </main>
-  </>
-)
-
+  return (
+    <>
+      <nav>ag.dev</nav>
+      <main className="flex flex-col bg-base-300 max-w-xl mx-auto my-auto items-center border min-h-xl">
+        <div className="landing-page-profile-img">
+          <img alt-loading="eager" src="/images/portfolio.png" alt="landing-page-profile-pic" width='200' height='200' />
+        </div>
+        <div className="flex flex-col items-center">
+          <h1>Alec Guyer</h1>
+          <p>Full-Stack Software Developer</p>
+        </div>
+        <div className="divider divider-accent" />
+        <div className="flex flex-col items-center">
+          <h2>Fluid and Responsive UI Experiences</h2>
+          <h2>Future-Proof Backend Systems</h2>
+          <a className="btn hover:btn-primary" href="/projects">Step into the matrix!</a>
+        </div>
+        <div className="divider divider-accent" />
+        <div className="flex">
+        <a className="btn hover:btn-primary" href="https://github.com/GrayGrimm">GitHub</a>
+        <a className="btn hover:btn-primary" href="https://www.linkedin.com/in/alec-guyer">LinkedIn</a>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
