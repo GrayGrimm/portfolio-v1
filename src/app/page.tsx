83a8713f700858e02f7f99ea6baf990577d0f379
guyera <thegraygrimm@graymansurvivaltech.com>
2025-11-13 17:39:16 -0500

    landing page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,19 @@ const Home = () => {
           <img alt-loading="eager" src="/images/portfolio.png" alt="landing-page-profile-pic" width='200' height='200' />
         </div>
         <div className="flex flex-col items-center">
-          <h1>Alec Guyer</h1>
+          <h1 className="font-bold text-2xl">Alec Guyer</h1>
           <p>Full-Stack Software Developer</p>
         </div>
         <div className="divider divider-accent" />
-        <div className="flex flex-col items-center">
-          <h2>Fluid and Responsive UI Experiences</h2>
-          <h2>Future-Proof Backend Systems</h2>
-          <a className="btn hover:btn-primary" href="/projects">Step into the matrix!</a>
+        <div className="flex flex-col items-center gap-2">
+          <h2 className="font-bold">Fluid and Responsive UI Experiences</h2>
+          <h2 className="font-bold">Future-Proof Backend Systems</h2>
+          <a className="btn hover:btn-primary" href="/projects">View my Projects</a>
         </div>
         <div className="divider divider-accent" />
-        <div className="flex">
-        <a className="btn hover:btn-primary" href="https://github.com/GrayGrimm">GitHub</a>
-        <a className="btn hover:btn-primary" href="https://www.linkedin.com/in/alec-guyer">LinkedIn</a>
+        <div className="flex gap-2">
+        <a className="btn bg-black text-white border-black" href="https://github.com/GrayGrimm"><img width='32' height='32' src='/icons/github-mark-white.png' />GitHub</a>
+        <a className="btn bg-[#0967C2] text-white border-[#0059b3]" href="https://www.linkedin.com/in/alec-guyer"><img width='32' height='32' src='/icons/InBug-White.png' />LinkedIn</a>
         </div>
       </main>
     </>
